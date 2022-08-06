88c131c1c829c22612ed0a07a3554ac0bd13b137
Brendan <brendanmarko@gmail.com>
2022-08-06 15:04:54 -0400

    Base

--- scraper.py
+++ scraper.py
@@ -13,6 +13,8 @@ DEBUG_TAG="[Scraper]:"
 
 BUFFER="buffer.txt"
 
+URL="https://www.cibc.com/en/interest-rates/mortgage-rates.html"
+
 class Scraper(object):
     def __init__(self):
 
@@ -30,19 +32,13 @@ class Scraper(object):
 
         # Run Browser operations
         with open(filename, "r") as f:
-            for line in f:
-                print("Processing: " + str(line.strip()))
-                try:
-                    browser=self.createBrowserInstance()
-                    self.executeQuery(browser, line)
-                    self.closeBrowserInstance(browser)
-                except:
-                    print("Issue scraping device: " + line.strip())
-                    self.closeBrowserInstance(browser)
-                    self.invalid.append(line.strip())
-
-        # View results
-        self.viewResults(filename)
+            try:
+                browser=self.createBrowserInstance()
+                self.executeQuery(browser, NULL)
+                self.closeBrowserInstance(browser)
+            except:
+                print("Issue scraping device: " + line.strip())
+                self.closeBrowserInstance(browser)
 
     # Creates Browser instance
     def createBrowserInstance(self):
@@ -55,11 +51,12 @@ class Scraper(object):
 
     # Executes Browser queries
     def executeQuery(self, browser, ip):
-        browser.get("http://" + str(ip) + "/basic")
+        browser.get(URL)
         browser.refresh()
 
         # Gather data
         xpath="//*[@id='Status']/table"
+        # xpath="//*[@id='Status']/table"
 
         # Parses data
         result=self.processOutput(WebDriverWait(browser, 20).until(EC.element_to_be_clickable((By.XPATH, xpath))))
@@ -69,41 +66,15 @@ class Scraper(object):
 
     # Writes data to temp file and parses it
     def processOutput(self, data):
-        m=""
-        p=""
-        s=""
-
         with open(BUFFER, "w+") as w:
             w.write(data.text)
 
         with open(BUFFER, "r") as r:
             for line in r:
-                if ("Product Name" in line and "Serial Number" in line):
-                    line=line.replace("Serial Number",":Serial Number").split(':')
-                    if ("Product Name" == line[0] and "Serial Number" == line[2]):
-                        p=line[1].strip()
-                        s=line[3].strip()
-                if ("MAC Address" in line):
-                    line=line.replace("MAC Address",":MAC Address").split(':')
-                    if ("MAC Address" == line[2] or ":MAC Address" == line[2]):
-                        m=line[3].strip()
-                    elif ("MAC Address" == line[1]):
-                        m=line[2].strip().split(" ")[0]
-
-        # Returns data as tuple
-        return (p,s,m)
+                print(str(line))
 
     def closeBrowserInstance(self, browser):
         browser.quit()
-
-    def viewResults(self, filename):
-        with open("results/" + filename.replace(".txt", "") + "_results.csv", "w") as f:
-            for r in self.results:
-                f.write(r[0].strip() + "," + str(r[1][0]) + "," + str(r[1][1]) + "," + str(r[1][2]) + "\n")
-
-    def viewInvalid(self):
-        for i in self.invalid:
-            print("[" + i + "]")
 
 # Execute
 s=Scraper()
