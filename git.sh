07ef40af62e82b8c1ad1bb82ed74b709b9a2f88b
Brendan <brendanmarko@gmail.com>
2022-08-07 02:06:48 -0400

    Base

--- git.sh
+++ git.sh
@@ -1,4 +1,6 @@
 git add .
 git commit -m "Base"
-git push https://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@github.com/brendanmarko/Scraper.git
+key=echo ../git_token/current.key
+echo $key
+git push https://$key@github.com/brendanmarko/Scraper.git
 echo "[GIT COMPLETED]"
