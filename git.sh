29476d48a350c0427d23e856568bf27ff9367257
Brendan <brendanmarko@gmail.com>
2022-08-07 02:07:44 -0400

    Base

--- git.sh
+++ git.sh
@@ -1,6 +1,6 @@
 git add .
 git commit -m "Base"
-key=echo ../git_token/current.key
+key=cat ../git_token/current.key
 echo $key
 git push https://$key@github.com/brendanmarko/Scraper.git
 echo "[GIT COMPLETED]"
